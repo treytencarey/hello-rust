6a642a42e81b682ce66683f092657039f14f6a0e
treytencarey <treytencarey@gmail.com>
2024-05-21 22:18:21 -0500

    initial bevy_ecs_tilemap integration

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ use bevy::prelude::*;
 
 use crate::network_main::NetworkMain;
 use crate::script_main::ScriptMain;
+use crate::tiles_main::TilesMain;
 mod network_main;
 mod script_main;
+mod tiles_main;
 
 mod client;
 mod protocol;
 mod server;
 mod settings;
 mod shared;
+mod helpers;
 
 fn main() {
     let mut app = App::new()
-        .add_plugins((NetworkMain, ScriptMain))
+        .add_plugins((NetworkMain, ScriptMain, TilesMain))
         .run();
 }
---
+++ src/tiles_main.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+use crate::helpers::tiled;
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let map_handle: Handle<tiled::TiledMap> = asset_server.load("map_1.tmx");
+
+    commands.spawn(tiled::TiledMapBundle {
+        tiled_map: map_handle,
+        ..Default::default()
+    });
+}
+
+pub(crate) struct TilesMain;
+impl Plugin for TilesMain {
+    fn build(&self, app: &mut App) {
+        // Add your network systems, resources, etc. here
+        app.add_plugins(TilemapPlugin)
+            .add_plugins(tiled::TiledMapPlugin)
+            .add_systems(Startup, startup);
+    }
+}
